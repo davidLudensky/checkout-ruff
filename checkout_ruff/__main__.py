5e24a36bdd01927f4c79cf21a112f7b75f5e38d9
david_lud <ludensky.david@gmail.com>
2024-06-22 00:53:49 +0300

    feature: changed main logic

---
+++ checkout_ruff/__main__.py
@@ -0,0 +1,4 @@
+from checkout_ruff.hello_world import say_hello
+
+if __name__ == "__main__":
+    print(say_hello())
--- checkout_ruff/hello_world.py
+++ checkout_ruff/hello_world.py
@@ -1,7 +1,3 @@
 # hello_world_poetry/hello_world.py
 def say_hello():
     return "Hello, World!"
-
-
-if __name__ == "__main__":
-    print(say_hello())
